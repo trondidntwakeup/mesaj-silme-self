6a2d12b5c39c8646890733f2629fb0c3cf87c64e
tron <165304758+trondidntwakeup@users.noreply.github.com>
2025-03-24 00:24:01 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off
+cd
+color a
+title developed by tron
+node tron.js
+pause
---
+++ tron.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js-selfbot-v13');
+
+// TOKEN YAPISTIR GARDASIM BENİM SENİN O ALLAHINI SEVERİM
+const token = "";
+
+const client = new Discord.Client({
+    checkUpdate: false, 
+});
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} olarak giriş yapıldı.`);
+});
+
+client.on('messageCreate', async (message) => {
+    
+    if (message.author.id !== client.user.id) return;
+
+    
+    if (message.content.startsWith('!sil')) {
+        const args = message.content.split(' ');
+        const limit = parseInt(args[1], 10) || 10; 
+
+        const messages = await message.channel.messages.fetch({ limit: 100 });
+        const userMessages = messages.filter(msg => msg.author.id === client.user.id).first(limit);
+
+        for (const msg of userMessages) {
+            await msg.delete().catch(console.error);
+        }
+
+        console.log(`Son ${limit} mesaj silindi.`);
+    }
+});
+
+
+client.login(token);
